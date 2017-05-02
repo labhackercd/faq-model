7e9851b98da02f63e1c6bc9fc9f254d9466a8aa7
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-05-02 19:09:45 -0300

    shit
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,24 @@ $('.faq__question').click(function() {
   $(this).siblings('.faq__answer').toggleClass('active');
   $(this).find('.faq__question-icon').toggleClass('active');
 });
+
+function setTab(hash) {
+  $('.faq').addClass('hide');
+  $(hash).removeClass('hide');
+}
+
+$('.navigation__link').click(function(event) {
+  setTab(event.target.hash);
+})
+
+
+$(document).ready(function() {
+  var actualHash = document.location.hash;
+
+  if (actualHash) {
+    setTab(actualHash);
+  } else {
+    var defaultHash = $('.navigation__link').first()[0].hash;
+    setTab(defaultHash);
+  }
+})
